0e2d1629f1411e71cb51e5a7ebf84c2e8c4cdc4d
Crowdbotics <team@crowdbotics.com>
2020-10-24 21:27:18 +0000

    Update models

--- backend/rich_test_21924/wsgi.py
+++ backend/rich_test_21924/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rich_test_21924.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rich_test_21924.settings")
 
 application = get_wsgi_application()
